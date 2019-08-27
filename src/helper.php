1799b88dbf3ed0931184d45908b55acdc9945f80
fudanda <314553951@qq.com>
2019-08-27 18:30:49 +0800

    1.修改页面错位

--- src/helper.php
+++ src/helper.php
@@ -216,3 +216,27 @@ if (!function_exists('buildTree')) {
         return $tree;
     }
 }
+if (!function_exists('cc_format')) {
+    /**
+     * 一个字符串中的大写转换成 _+小写的方式
+     * @param [String] $name 字符串
+     * @return void
+     */
+    function cc_format($name)
+    {
+        $temp_array = array();
+        for ($i = 0; $i < strlen($name); $i++) {
+            $ascii_code = ord($name[$i]);
+            if ($ascii_code >= 65 && $ascii_code <= 90) {
+                if ($i == 0) {
+                    $temp_array[] = chr($ascii_code + 32);
+                } else {
+                    $temp_array[] = '_' . chr($ascii_code + 32);
+                }
+            } else {
+                $temp_array[] = $name[$i];
+            }
+        }
+        return implode('', $temp_array);
+    }
+}
--- static/admin/js/lay-module/layuimini/layuimini.js
+++ static/admin/js/lay-module/layuimini/layuimini.js
@@ -648,6 +648,32 @@ layui.define(["element", "jquery", "layer"], function (exports) {
         sessionStorage.setItem('layuiminiBgcolorId', bgcolorId);
         layuimini.initBgColor();
     });
+    //layer 打开页面
+    window.WeAdminShow = function (title, url, w, h) {
+        if (title == null || title == '') {
+            title = false;
+        };
+        if (url == null || url == '') {
+            url = "404.html";
+        };
+        if (w == null || w == '') {
+            w = ($(window).width() * 0.95);
+        };
+        if (h == null || h == '') {
+            h = ($(window).height() - 100);
+        };
+
+        layer.open({
+            type: 2,
+            area: [w + 'px', h + 'px'],
+            fix: false, //不固定
+            maxmin: true,
+            shadeClose: true,
+            shade: 0.4,
+            title: title,
+            content: url,
+        });
+    }
 
     exports("layuimini", layuimini);
 }
